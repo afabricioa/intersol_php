88a1adfb4d6cb6bd2cdd01d345e151b0d3baa6d6
Fabricio <fabricio.a.s@hotmail.com>
2020-02-09 12:52:17 -0300

    Update conexao.php

--- metodos/conexao.php
+++ metodos/conexao.php
@@ -27,4 +27,3 @@ $conn = mysqli_connect($hostname, $username, $password, $database);
 if (!$conn) {
     die("Connection failed: " . mysqli_connect_error());
 }
-echo "Connection was successfully established!";
