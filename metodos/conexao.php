e7435b4916b8a93b38fa64dee5ea5e0f1e057874
Fabricio <fabricio.a.s@hotmail.com>
2020-02-08 21:52:28 -0300

    conexão com o heroku
    adicionado o banco de dados do heroku

--- metodos/conexao.php
+++ metodos/conexao.php
@@ -1,9 +1,9 @@
 <?php
 // conexão com o banco de dados
-$servername = "localhost:8889";
-$username = "root";
-$password = "root";
-$db_name ="crudPHP_intersol";
+$servername = "izm96dhhnwr2ieg0.cbetxkdyhwsb.us-east-1.rds.amazonaws.com	";
+$username = "doax53ajdh8sc24h";
+$password = "b9qu7b5f6cwwnqv4";
+$db_name ="fit0wj9lgpd3rb53";
 
 $connect = mysqli_connect($servername, $username, $password, $db_name);
 //mysqli_set_charset($connect, "uft-8");
